adfce079aa0b0507a2e710d308529de1c202b934
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2022-06-26 14:25:32 +0900

    first commit

---
+++ dataDisplay/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  dataDisplay
+//
+//  Created by 副山俊輔 on 2022/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ dataDisplayUITests/dataDisplayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  dataDisplayUITestsLaunchTests.swift
+//  dataDisplayUITests
+//
+//  Created by 副山俊輔 on 2022/06/26.
+//
+
+import XCTest
+
+class dataDisplayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
